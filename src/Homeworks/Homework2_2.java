3881b496bcf8b75f253c3f96b78bb8c71f5e1dc4
Fink <elhost@list.ru>
2021-04-02 22:32:24 +0300

    ДЗ 2. Типы данных. Вариант 2.

---
+++ src/Homeworks/Homework2_2.java
@@ -0,0 +1,12 @@
+package Homeworks;
+
+public class Homework2_2 {
+    public static void main(String[] args) {
+        double fractional;
+        fractional = 10.3247;
+        byte integer;
+        integer = 127;
+        System.out.println(fractional);
+        System.out.println(integer);
+    }
+}
