22cdd9d19342a5dfe9c487ec609963b68cdfd0d3
Igor_Sidorov <igor.sidorov.190795@gmail.com>
2021-04-18 16:35:24 +0300

    Add comments for 11 task

--- src/Homeworks/Homework11.java
+++ src/Homeworks/Homework11.java
@@ -8,7 +8,10 @@ public class Homework11 {
         System.out.println("Введите числа:");
         String str = in.nextLine();
         int number = in.nextInt();
+        //Выпадает исключение, если у нас переменная str содержит не целое число (к примеру - 11.11)
+        //Еще один момент => лучше всего преобразовать два числа к типу double и сравнить через compareTo, как ты делал
         Integer x = Integer.parseInt(str);
+        //Autoboxing тут не надо явно организовывать, это неявно сам jvm сделает
         Integer y = number;
         int result = y.compareTo(x);
         switch (result) {
