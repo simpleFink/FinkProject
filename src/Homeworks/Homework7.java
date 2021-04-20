28c29d60ed939009461a5122ab5a16b1c8ac4cdc
Fink <elhost@list.ru>
2021-04-20 20:23:00 +0300

    ДЗ 7. Внес исправления по комментариям.

--- src/Homeworks/Homework7.java
+++ src/Homeworks/Homework7.java
@@ -4,29 +4,31 @@ import java.util.Scanner;
 
 public class Homework7 {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 6;
-        int z = 12;
+        String x = "5";
+        String y = "Hello";
+        String z = "Привет кот";
         Scanner size = new Scanner(System.in);
-        System.out.println("Выберите размер массива: ");
+        System.out.println("Выберите размер массива:");
         int arraysize = size.nextInt();
         //Смотри, по условию у нас нет ограничения на тип введенных данных
         //(это может быть вещественное число или просто какая-то строка)
         //попробуй адаптировать программу под ввод любых данных
-        int array[] = new int[arraysize];
-        System.out.println("Введите элементы массива: ");
+        String array[] = new String[arraysize];
+        System.out.println("Введите элементы массива:");
         for (int i = 0; i < arraysize; i++) {
             //тут тоже нужно будет поменять метод у сканера
-            array[i] = size.nextInt();
+            Scanner data = new Scanner(System.in);
+            array[i] = data.nextLine();
         }
-        System.out.println("Введенный массив: ");
+        System.out.println("Введенный массив:");
         for (int i = 0; i < arraysize; i++) {
-            System.out.print(array[i] + " ");
+            System.out.print(array[i] + ", ");
         }
-        for (int array1 : array) {
-            if (array1 == x | array1 == y | array1 == z) {
+        for (String array1 : array) {
+            if (array1.equals(x)| array1.equals(y) | array1.equals(z)) {
                 System.out.println();
                 System.out.println("Данное значение имеется в константах");
+                break;
             }
         }
     }
