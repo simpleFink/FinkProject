2b4008997ecb4a86d26a81e6a58e03ec8428e054
Fink <elhost@list.ru>
2021-04-27 11:12:33 +0300

    Итоговая задача № 4.

---
+++ src/Homeworks/FinalHomework4.java
@@ -0,0 +1,34 @@
+package Homeworks;
+
+import java.util.Scanner;
+
+public class FinalHomework4 {
+    public static void main (String [] args) {
+        System.out.println ("Загадка: Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+        Scanner in = new Scanner (System.in);
+        for (int i = 1; i <= 4; i++) {
+            System.out.print("Ответ: ");
+            String answer = in.nextLine();
+            if ((i >= 3) & (!answer.equals("Подсказка") & (!answer.equals("Заархивированный вирус")))) {
+                System.out.println("Обидно, приходи в другой раз");
+                break;
+            }
+            if (answer.equals("Подсказка") & (i >= 2)) {
+                System.out.println("Подсказка уже недоступна");
+                continue;
+            }
+            switch (answer) {
+                case ("Заархивированный вирус"):
+                    System.out.println ("Правильно!");
+                    System.exit(0);
+                    break;
+                case ("Подсказка"):
+                    System.out.println ("Подсказка: .zip worm");
+                    i = 3;
+                    break;
+                default:
+                    System.out.println("Подумай еще!");
+            }
+        }
+    }
+}
